58383b13d19544ca6a486d0021dc9bdd77890a1d
Victor Lourng <hello@victorlourng.com>
2014-10-01 01:28:17 -0400

    Week 3 Assignments

---
+++ Week 3 - Fundamentals of Programming/3.4 Program It - Hello World.java
@@ -0,0 +1,47 @@
+/* Author: Victor Lourng
+
+School: Lower Moreland HS
+
+Project: Hello World!
+
+Date: September 23, 2014 */
+
+public class HelloWorld {
+	
+	public static void main(String [] args) {
+		
+		System.out.println("   ");
+		System.out.println("        ___         ___           ___           ___       ");
+		System.out.println("       /|  |       /|  |         /|__|         /|  |      ");
+		System.out.println("       |:|  |     /::|  |       /:/  /        /::|  |     ");
+		System.out.println("   ___ /::|__|   /:/|:|  |     /:/  /        /:/|:|  |    ");
+		System.out.println("  /|  /:/|/__/  /::|~|:|  |   /:/__/  ___   /::|~|:|  |   ");
+		System.out.println("  |:|/:/  /    /:/|:| |:|__|  |:|  | /|__| /:/|:| |:|__|  ");
+		System.out.println("   |::/  /     |/__|:|/:/  /  |:|  |/:/  / |/__|:|/:/  /  ");
+		System.out.println("    |/__/           |::/  /   |:|__/:/  /       |::/  /   ");
+		System.out.println("                    /:/  /     |::::/__/        /:/  /    ");
+		System.out.println("                   /:/  /       ~~~~           /:/  /     ");
+		System.out.println("                   |/__/                       |/__/      ");
+		System.out.println("   ");
+		System.out.println("  _____ _______  ");
+		System.out.println("    |   |______  ");
+		System.out.println("  __|__ ______|  ");
+		System.out.println("                 ");
+		System.out.println("   ");
+		System.out.println("   ");
+		System.out.println("   _____    _   _   _   _     _      ");
+		System.out.println("  |' ___|U |'|u| | | | |'|  U|'|u    ");
+		System.out.println(" U| |_  u || ||| |<|  || |> || |/    ");
+		System.out.println(" ||  _|/   | |_| |U| ||  |u  |_|     ");
+		System.out.println("  |_|     <<|___/  |_| |_|   (_)     ");
+		System.out.println("  )(||,- (__) )(   ||   ||,-.|||_    ");
+		System.out.println(" (__)(_/     (__)  (_')  (_/(__)_)   ");
+		System.out.println("   ");
+		System.out.println("   ");
+		System.out.println("   ");
+		System.out.println(" Created by Victor Lourng  ");
+		System.out.println(" Using http://patorjk.com/software/taag-v1/  ");
+
+	}
+	
+}
