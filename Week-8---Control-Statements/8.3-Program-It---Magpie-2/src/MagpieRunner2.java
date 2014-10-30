b5049aac18ad29e54926409ac1abbda957506a0d
Victor Lourng <hello@victorlourng.com>
2014-10-30 13:23:43 -0400

    My version, with nuzzles and huggles!

--- Week-8---Control-Statements/8.3-Program-It---Magpie-2/src/MagpieRunner2.java
+++ Week-8---Control-Statements/8.3-Program-It---Magpie-2/src/MagpieRunner2.java
@@ -1,29 +1,153 @@
-import java.util.Scanner;
-
 /**
- * A simple class to run the Magpie class.
+ * A program to carry on conversations with a human user.
+ * This is the initial version that:  
+ * <ul><li>
+ *       Uses indexOf to find strings
+ * </li><li>
+ * 		    Handles responding to simple words and phrases 
+ * </li></ul>
+ * This version uses a nested if to handle default responses.
  * @author Laurie White
  * @version April 2012
  */
-public class MagpieRunner2
+public class Magpie2
 {
-
 	/**
-	 * Create a Magpie, give it user input, and print its replies.
+	 * Get a default greeting 	
+	 * @return a greeting
 	 */
-	public static void main(String[] args)
+	public String getGreeting()
 	{
-		Magpie2 maggie = new Magpie2();
-		
-		System.out.println (maggie.getGreeting());
-		Scanner in = new Scanner (System.in);
-		String statement = in.nextLine();
-		
-		while (!statement.equals("Bye"))
+		return "Hello, let's talk.";
+	}
+	
+	/**
+	 * Gives a response to a user statement
+	 * 
+	 * @param statement
+	 *            the user statement
+	 * @return a response based on the rules given
+	 */
+	public String getResponse(String statement)
+	{
+		String response = "";
+		if (statement.indexOf("no") >= 0)
 		{
-			System.out.println (maggie.getResponse(statement));
-			statement = in.nextLine();
+			response = "Why so negative? :-(";
 		}
+		else if (statement.indexOf("yes") >= 0)
+		{
+			response = "Yay! :-D";
+		}
+		else if (statement.indexOf("maybe") >= 0)
+		{
+			response = "Oh really? Pick a side! :-?";
+		}
+		else if (statement.indexOf("exist") >= 0)
+		{
+			response = "I exist for however long you want me to. ;-)";
+		}
+		else if (statement.indexOf("name") >= 0)
+		{
+			response = "Nice to meet you! I'm Magpie. ;-)";
+		}
+		else if (statement.indexOf("nuzzle") >= 0)
+		{
+			response = "*nuzzles back* :-B";
+		}
+		else if (statement.indexOf("mother") >= 0
+				|| statement.indexOf("father") >= 0
+				|| statement.indexOf("mom") >= 0
+				|| statement.indexOf("dad") >= 0
+				|| statement.indexOf("sister") >= 0
+				|| statement.indexOf("brother") >= 0)
+		{
+			response = "Tell me more about your family. :-O";
+		}
+		else if (statement.indexOf("dog") >= 0
+				|| statement.indexOf("cat") >= 0)
+		{
+			response = "Tell me more about your pets. :-O";
+		}
+		else if (statement.indexOf("Nelson") >= 0
+				|| statement.indexOf("Yuka") >= 0)
+		{
+			response = "She sounds like good teacher. :-)";
+		}
+		else if (statement.length() < 1)
+		{
+			response = "Say something, please. ;-P";
+		}
+		else if (statement.indexOf("Athos") >= 0
+				|| statement.indexOf("Porthos") >= 0
+				|| statement.indexOf("Aramis") >= 0)
+		{
+			response = "Ah, the three musketeers. Love them! /-)";
+		}
+		else if (statement.indexOf("weather") >= 0
+				|| statement.indexOf("sunny") >= 0
+				|| statement.indexOf("cloudy") >= 0
+				|| statement.indexOf("wind") >= 0
+				|| statement.indexOf("rain") >= 0
+				|| statement.indexOf("hot") >= 0
+				|| statement.indexOf("cold") >= 0
+				|| statement.indexOf("mild") >= 0
+				|| statement.indexOf("cool") >= 0)
+		{
+			response = "Weather nerd! '-)";
+		}
+		else if (statement.indexOf("dinosaurs") >= 0
+				|| statement.indexOf("floppy") >= 0
+				|| statement.indexOf("tamagotchi") >= 0)
+		{
+			response = "They don't make things like these any more. :-/";
+		}
+		else if (statement.indexOf("happy") >= 0
+				|| statement.indexOf("awesome") >= 0
+				|| statement.indexOf("great") >= 0)
+		{
+			response = "Glad to hear! I'm doing awesome as well. :-D";
+		}
+		else if (statement.indexOf("sad") >= 0
+				|| statement.indexOf("cry") >= 0
+				|| statement.indexOf("worried") >= 0)
+		{
+			response = "I'm sorry to hear that. :'(";
+		}
+		else
+		{
+			response = getRandomResponse();
+		}
+		return response;
 	}
 
+	/**
+	 * Pick a default response to use if nothing else fits.
+	 * @return a non-committal string
+	 */
+	private String getRandomResponse()
+	{
+		final int NUMBER_OF_RESPONSES = 4;
+		double r = Math.random();
+		int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
+		String response = "";
+		
+		if (whichResponse == 0)
+		{
+			response = "Interesting, tell me more. :-O";
+		}
+		else if (whichResponse == 1)
+		{
+			response = "Hmmm. :-O";
+		}
+		else if (whichResponse == 2)
+		{
+			response = "Do you really think so? :-O";
+		}
+		else if (whichResponse == 3)
+		{
+			response = "You don't say. :-O";
+		}
+		return response;
+	}
 }
